d31a807c43e7a3f6f4f41871881e4dbef7b8866e
bhagyasree <sunkaribsree@gmail.com>
2024-09-02 23:34:06 +1000

    updating again

--- 12-functions.sh
+++ 12-functions.sh
@@ -6,21 +6,20 @@ then
     echo "you are not root user at this moment. please try being a root user"
     exit 1
 fi
-
 dnf list installed mysql
 if [ $? -ne 0 ]
 then
-    echo "mysql package is not installed yet. will install now"
+    echo "mysql is not seen in the installed packages list.hence installing it.."
     dnf install mysql -y
     if [ $? -ne 0 ]
     then
-        echo "installation process has failed. please check your shell script"
+        echo "mysql package installation has failed. please check the shellscript again"
         exit 1
     else
-        echo "installation is successful."
+        echo "mysql successfully installed"
     fi
 else
-    echo "mysql package is already installed.nothing to do"
+    echo "mysql package is already installed.Nothing to do"
 fi
 
 dnf list installed nginx
