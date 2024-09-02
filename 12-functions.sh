94e0fe9aae3ceea1b5bb7937034b6e3b35b8d96d
bhagyasree <sunkaribsree@gmail.com>
2024-09-02 23:36:32 +1000

    updating again

--- 12-functions.sh
+++ 12-functions.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/bin/bash/
+
 USER=$(id -u)
-echo "your user id is : $USER"
+echo "your user id is: $USER"
 if [ $USER -ne 0 ]
 then
-    echo "you are not root user at this moment. please try being a root user"
+    echo "you are not currently in root user access. please try again with root user access"
     exit 1
 fi
 dnf list installed mysql
@@ -13,27 +14,11 @@ then
     dnf install mysql -y
     if [ $? -ne 0 ]
     then
-        echo "mysql package installation has failed. please check the shellscript again"
+        echo "mqsql package installation has failed. please check the shellscript again"
         exit 1
     else
         echo "mysql successfully installed"
     fi
 else
     echo "mysql package is already installed.Nothing to do"
-fi
-
-dnf list installed nginx
-if [ $? -ne 0 ]
-then
-    echo "nginx is not seen in the installed packages list.hence installing it.."
-    dnf install nginx -y
-    if [ $? -ne 0 ]
-    then
-        echo "nginx package installation has failed. please check the shellscript again"
-        exit 1
-    else
-        echo "nginx successfully installed"
-    fi
-else
-    echo "nginx package is already installed.Nothing to do"
 fi
