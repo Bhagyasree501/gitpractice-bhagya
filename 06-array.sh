9bc7c78a0b7ccfd7572399789a6a1b2ff0a0a200
bhagyasree <sunkaribsree@gmail.com>
2024-09-02 15:56:35 +1000

    uderstadning how to declare an array

---
+++ 06-array.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+FRUITS=["plum" "apple" "orange"] 
+# In any array, index value starts with 0
+#In above array, size of the array is 3
+echo "The first fruit is ${FRUITS[0]}"
+echo "The second fruit is ${FRUITS[1]}"
+echo "All fruits in this array are ${FRUIT[@]}"
