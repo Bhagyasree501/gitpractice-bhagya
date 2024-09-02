fdf878a8c476d3b2acaabb67cc30d4bb5abaa30b
bhagyasree <sunkaribsree@gmail.com>
2024-09-02 15:05:08 +1000

    understanding variables concept

---
+++ 03-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+PERSON1 = ramesh
+PERSON2 =suresh
+
+echo "$PERSON1: hi ${PERSON2}"
+echo "$PERSON2: hi $PERSON1 long time, how are you"
+echo "$PERSON1: yea $PERSON2, how are you these days"
