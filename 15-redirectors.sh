ef1a322ef404a5c94cab105e36f920308dc9fe78
bhagyasree <sunkaribsree@gmail.com>
2024-09-03 17:56:54 +1000

    understanding redirecting and appending to files concepts

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -8,8 +8,8 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-echo "$TIMESTAMP"
 FILE_NAME="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+echo "$FILE_NAME"
 mkdir -p $LOGS_FOLDER
 
 
