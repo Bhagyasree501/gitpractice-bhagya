cd769e77cbeec80367124d0f14bf1dc2c2d320da
bhagyasree <sunkaribsree@gmail.com>
2024-09-03 18:06:43 +1000

    added if condition to exit script, if user did not provide atleast 1 uesr name

--- 15-redirectors.sh
+++ 15-redirectors.sh
@@ -9,7 +9,7 @@ LOGS_FOLDER="/var/log/shell-script"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 FILE_NAME="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
-echo "$FILE_NAME"
+echo "$FILE_NAMEz"
 mkdir -p $LOGS_FOLDER
 
 
@@ -33,14 +33,20 @@ VALIDATE(){
 }
 
 CHECK_ROOT #I am calling CHECK_ROOT function
+if [ $@ -eq 0 ]
+then
+    echo -e "$R hey!looks like no agruments are provided by you.Make sure you provide atleast one package name as argument $N"
+    echo -e "$R For example, sudo sh <filename> package1 package2 ...$N"
+    exit 1
+fi
 
 for PACKAGE in $@ # all arugemttns provided by user are saved into #@. variable 'PACKAGE' picks one arg at a time while suing for loop.
 do
-dnf list installed $PACKAGE
+dnf list installed $PACKAGE &>>$FILE_NAME
 if [ $? -ne 0 ]
 then
     echo -e "$Y hey!looks like $PACKAGE is not installed.let me install it now $N" &>>$FILE_NAME
-    dnf install $PACKAGE -y
+    dnf install $PACKAGE -y &>>$FILE_NAME
     VALIDATE $? "$PACKAGE" # I am calling validate function here
 else
     echo -e "$B hey! looks like $PACKAGE is already installed.nothing to do $N" &>>$FILE_NAME
