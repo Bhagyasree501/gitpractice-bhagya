348cfb4995d36999e3067d2f32c414abbd3c783d
bhagyasree <sunkaribsree@gmail.com>
2024-09-12 13:11:02 +1000

    updating shellscript

--- 17-deleteoldlogs.sh
+++ 17-deleteoldlogs.sh
@@ -17,4 +17,15 @@ fi
 
 LOG_FILES=$(find $DIRPATH -name "*.log" -mtime +14)
 echo "the list of files which are older then 14 days are:$LOG_FILES"
- 
+
+while IFS= read -r logfile
+do
+    echo "the file we are deleting is $logfile"
+    rm -rf $logfile
+done <<<$LOG_FILES # this is the variable holding the list of  filtered .log files and this variable is provided as inpt to whileloop
+
+ # IFS=Internal Field Separator
+ # IFS by default uses space,tab,next line for spearating fields while parsing input
+ # we can change the IFS to , also as per our requirement
+ # read -r means reads the input , i mean $LOG_FILES without considering escape characters. Instead it will consider them literally
+ #and puts it into variable 'logfile'
