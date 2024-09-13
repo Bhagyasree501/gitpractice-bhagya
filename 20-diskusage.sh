423300a0376087089a6acc097fd048af631721ff
bhagyasree <sunkaribsree@gmail.com>
2024-09-13 16:48:49 +1000

    trying to send error message when diskusage of filesystem crosses 5%

--- 20-diskusage.sh
+++ 20-diskusage.sh
@@ -8,7 +8,7 @@ while IFS= read -r line
 do
     FOLDERNAME=$(echo $line | awk -F " " '{print $NF}')
     USAGE=$(echo $line | awk -F " " '{print $6F}' | cut -d "%" f1)
-    if [ "$USAGE" -ge "$THRESHOLD"]
+    if [ $USAGE -ge $THRESHOLD ]
     then
         echo "the storage used by $FOLDERNAME is: $USAGE. And this vlue is grated than $THRESHOLD "
     else 
